83fc7ce04f456adfe84f8a56796f30eea566e21b
Rupa2298 <138680466+Rupa2298@users.noreply.github.com>
2024-07-18 13:12:02 +0530

    Javascript
    18/7/2024

---
+++ src/JavaScript/ForLoop.jsx
@@ -0,0 +1,18 @@
+
+// //if else .......
+// // terminary 1+2 =3  ? true : false
+
+
+
+// // iterate ...............for  (initliazation;   condition;   Increament/decreament){........}
+
+
+// const number = parseInt(prompt("Enter a number :")) // 5 5*1=5 ..... 5x10=50
+
+// for (let i=1 ;i<=10 ; i++){
+
+//     const multiply = i * number;
+
+// console.log(`Multiplying Table of ${i} x ${number}= ${multiply}`)    
+
+// }
---
+++ src/JavaScript/IfElse.jsx
@@ -0,0 +1,33 @@
+// console.log("Hai Training")  //print -- inspect
+
+// //alert("Hello abinaya") // print -- current 
+
+// const num1 = parseInt(prompt('Enter a number 1 :'))  //parseInt -  string to number  // prompt  - print the number from user
+// const num2 = parseInt(prompt('Enter a Number 2 :'))
+
+// const add = num1+num2;
+
+// console.log(add)
+// console.log(`The sum of ${num1} and ${num2} is ${add}`) /// $  ``
+
+
+// //odd or even ------ if else  + - *  / %
+
+// const number = prompt('Enter a number : '); 
+
+// if(number % 2 == 0 ){    ///  '' ""  ``
+//   console.log(`The number ${number} is EVEN`)
+// } else{
+//     console.log(`The number ${number}  is ODD`)
+// }
+
+// //odd or even ------ if else  + - *  / %
+// // if(){....} else{}
+
+// const number1 = prompt('Enter a number : '); 
+
+// if(number % 2 == 0 ){    ///  '' ""  ``
+//   console.log(`The number ${number1} is EVEN`)
+// } else{
+//     console.log(`The number ${number1}  is ODD`)
+// }
