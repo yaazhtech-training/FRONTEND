c130db935b71dd2cdc43166017e1e378bb1182e2
Rupa2298 <138680466+Rupa2298@users.noreply.github.com>
2024-08-06 12:58:08 +0530

    Map
    6/8/2024

---
+++ src/JavaScript/Map.js
@@ -0,0 +1,12 @@
+// //key-value pair  ----- Example questions - Key{unique} ...... Answers - value {varible/different}
+
+// //key and value --- primitive / object[non-primitive]
+
+// let code = new Map();
+
+// code.set('rupa',25)
+// code.set('abi',25)
+// code.set('praveen',20)
+// code.set('praveen',22)
+// console.log(code.size);
+// console.log(code);
