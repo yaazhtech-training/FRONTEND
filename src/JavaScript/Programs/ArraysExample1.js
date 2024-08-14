b4c6e9e53441a24e0c6221d1d105b5b018aa8c30
SumiNarayanan <suminarayanan@outlook.com>
2024-08-14 15:21:13 +0300

    Filter and Sort
    14/8/24

---
+++ src/JavaScript/Programs/ArraysExample1.js
@@ -0,0 +1,34 @@
+//Filter Names  ---->Array Functions
+//-------------------------------------
+// const students=[
+//     {name:"Abi",age:1,grade:"A"},
+//     {name:"Ben",age:2,grade:"C"},
+//     {name:"sona",age:3,grade:"M"},
+//     {name:"Dev",age:1,grade:"B"},
+//     {name:"Jo",age:3,grade:"C"},
+//     ]
+    
+//     const getNames=students.map(stu=>stu.name)
+//     console.log(getNames)
+    
+ 
+    //Sort By Names
+    //--------------------------------------
+    // const students=
+    //     [
+    //     {name:"John",age:12,Section:"A"},
+    //     {name:"Abi",age:12,Section:"A"},
+    //      {name:"Zen",age:12,Section:"A"},
+    //      {name:"Mary",age:12,Section:"A"},
+    //      {name:"Princee",age:12,Section:"A"}
+    //       ]
+            
+    //  
+    //   const sortStudents=students.sort((a,b)=>a.name.localeCompare(b.name))
+    //   console.log(students)
+        
+    
+     
+    
+     
+     
