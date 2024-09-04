ccb6ef8dd7e221b92171288700c9edbb83160a72
Rupa2298 <138680466+Rupa2298@users.noreply.github.com>
2024-09-04 13:07:50 +0530

    Hoisting
    Hoisting

---
+++ src/JavaScript/Hoisting.js
@@ -0,0 +1,14 @@
+//hoisting
+
+
+//console.log(x); // undefined 
+//var x = 5; //
+//var x = 10; // globally scoped
+//let x = 5; //initiate //declare // blocked scoped
+//let x = 10
+//const x = 10
+//const x = 20
+//console.log(x); // 5 
+ 
+ 
+ //ES5 ----- ES6 let const
